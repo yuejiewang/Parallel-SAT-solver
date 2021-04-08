f264d0940b8a53c17604edfaa8bf193027e29e83
yuejiewang <kathywangyuejie@gmail.com>
2021-04-08 22:14:09 +0800

    add data struct utils and fake propagation function for test only

--- DPLL-parallel/bcp.h
+++ DPLL-parallel/bcp.h
@@ -8,18 +8,52 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<vector>
+#include<list>
 #include<string.h>
 #include<cstring>
 #include<string>
+#include<sstream>
 using namespace std;
 
 struct bitmap {
 	int maxlen;
-	vector<unsigned char> raw;
+	int rawlen;
+	unsigned char* raw;
 	bitmap(int _n) {
+		rawlen = _n;
 		maxlen = (_n + 7) / 8;
+		raw = new unsigned char[maxlen];
+		memset(raw, 0, maxlen * sizeof(unsigned char));
+	}
+	bitmap() {
+		maxlen = 0;
+		rawlen = 0;
+		raw = NULL;
+	}
+	void copy(bitmap _b) {
+		if (rawlen != _b.rawlen) {
+			rawlen = _b.rawlen;
+			maxlen = _b.maxlen;
+			raw = new unsigned char[maxlen];
+		}
 		for (int i = 0; i < maxlen; i++)
-			raw.push_back(0x00);
+			raw[i] = _b.raw[i];
+	}
+	int get_zero_index() {
+		for (int i = 0; i < maxlen; i++) {
+			if (raw[i] != 0xff) {
+				unsigned char c = raw[i];
+				for (int j = 0; j < 8; j++) {
+					if ((c & 0x01) != 0x01) {
+						int idx = i * 8 + j;
+						if (idx < rawlen) return idx;
+						return -1;
+					}
+					c = c >> 1;
+				}
+			}
+		}
+		return -1;
 	}
 	int get(int idx) {
 		if (idx >= maxlen * 8 || idx < 0) return 0;
@@ -44,21 +78,78 @@ typedef struct bitmap BITMAP;
 
 typedef vector<int> BITMAP_NAIVE;
 
-struct cnf_type {
-	vector<vector<int> > expr;
-	cnf_type(vector<vector<int> > vec): expr(vec) {}
-};
-typedef struct cnf_type CNF_TYPE;
+// maybe can use std::list here, faster on deletion
+typedef vector<string> CLAUSE_T;
+typedef vector<CLAUSE_T> CNF_T;  // when changing clause type change these two lines only
 
 typedef string CNF_NAIVE;
 
 struct node_t {
 	BITMAP dec;
 	BITMAP val;
-	CNF_NAIVE cnf;
-	node_t(BITMAP _d, BITMAP _v, CNF_NAIVE _c): dec(_d), val(_v), cnf(_c) {}
+	CNF_T cnf;
+	node_t(BITMAP _d, BITMAP _v, CNF_T _c): cnf(_c) {
+		dec.copy(_d);
+		val.copy(_v);
+	}
+	int remove_true_clauses_naive() {  // a naive implementation for deleting clauses whose value is already decided
+		if (cnf.empty()) exit(1);  // shouldn't be empty
+		for (CNF_T::iterator ii = cnf.begin(); ii != cnf.end();) {
+			if ((*ii).empty()) exit(1);  // shouldn't be empty
+			bool rm_flag = false;
+			for (CLAUSE_T::iterator ij = (*ii).begin(); ij != (*ii).end();) {
+				string var_str = *ij;
+				stringstream ss;
+				ss << var_str;
+				int var_idx;
+				ss >> var_idx;
+				if (var_idx < 0) {  // assuming clause variables start from index 1 (no zero)
+					var_idx = (~var_idx);  // var_idx = -var_idx
+					if (dec.get(var_idx)) {
+						if (val.get(var_idx)) (*ii).erase(ij);
+						else {
+							rm_flag = true;
+							break;
+						}
+					}
+					else ij++;
+				}
+				else {
+					var_idx--;
+					if (dec.get(var_idx)) {
+						if (!val.get(var_idx)) (*ii).erase(ij);
+						else {
+							rm_flag = true;
+							break;
+						}
+					}
+					else ij++;
+				}
+			}
+			if (rm_flag) cnf.erase(ii);
+			else if ((*ii).empty()) return 2;  // branch no-sat
+			else ii++;
+		}
+		if (cnf.empty()) return 1;  // branch sat
+		return 0;  // branch not finished
+	}
 };
 typedef struct node_t Node;
+
+int fake_propagation(Node* curr, Node* left, Node* right) {  // no propagation, for test only
+	if (curr == NULL) return -1;
+	int flag = curr->remove_true_clauses_naive();
+	if (flag == 0) {  // banch not finished
+		int idx = curr->dec.get_zero_index();
+		left->dec.set(idx, 1);
+		right->dec.set(idx, 1);
+		left->val.set(idx, 0);
+		right->val.set(idx, 1);
+		left->cnf = curr->cnf;
+		right->cnf = curr->cnf;
+	}
+	return flag;
+}
 
 // If we're going to implement more propagation algorithms we might need OOP
 // use the following for naive implementation
@@ -69,7 +160,7 @@ void propagation(Node* curr, Node* left, Node* right) {
 	 * left_DL/right_DL(pointer): the decision level of left/right child
 	 * function: compute and assign the value of left_DL and right_DL relatively
 	 * do Propagation() on the current node based on the decided variables, update bitmap (decided
-	 * and values) and the clause during the while loop, don't need to resolve conflict
+	 * and values) and the clause during the while loop
 	 * choose a split_variable, assign it with 0/1, update only this variable in the bitmap (decided
 	 * and values) and give the updated bitmaps and local clause to left/right child pointer
 	 * for the local clause, just remove all the variables assigned with a value to simplify the
--- DPLL-parallel/main.cpp
+++ DPLL-parallel/main.cpp
@@ -5,10 +5,7 @@
 	> Created Time: Thu Apr  8 11:35:03 2021
  ************************************************************************/
 
-#include<iostream>
 #include<omp.h>
-#include<sstream>
-#include<list>
 #include"bcp.h"
 using namespace std;
 static const int NTHREADS = 4;
@@ -18,45 +15,51 @@ int main()
 #pragma omp parallel num_threads(NTHREADS)
 	{
 #pragma omp for
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < NTHREADS; i++) {
 			int tid = omp_get_thread_num();
-			stringstream ss;
-			string str_tid, str_i;
-			ss << tid << " " << i;
-			ss >> str_tid >> str_i;
-			string str = str_i + " of thread " + str_tid + "\n";
 			BITMAP dec(8), val(8);
-			CNF_NAIVE cnf = str;
+			char str[128] = "1 2 3 4 5 6 7 8";
+			char* ptr = NULL;
+			char* substr = strtok_r(str, " ", &ptr);
+			CLAUSE_T clause;
+			while (substr != NULL) {
+				string tmp(substr);
+				clause.push_back(tmp);
+				substr = strtok_r(NULL, " ", &ptr);
+			}
+			CNF_T cnf;
+			cnf.push_back(clause);
 			Node* node = new Node(dec, val, cnf);
 			node->dec.set(0, 1);
-			node->dec.set(1, 0);
-			node->dec.set(2, 1);
-			node->dec.set(3, 0);
-			node->dec.set(4, 1);
-			node->dec.set(5, 0);
-			node->dec.set(6, 1);
-			node->dec.set(7, 0);
+			node->dec.set(1, 1);
+			node->val.set(0, tid & 0x1);
+			node->val.set(1, (tid >> 1) & 0x1);
 
-			node->val.set(0, 0);
-			node->val.set(1, 1);
-			node->val.set(2, 0);
-			node->val.set(3, 1);
-			node->val.set(4, 0);
-			node->val.set(5, 1);
-			node->val.set(6, 0);
-			node->val.set(7, 1);
 			local_stack[tid].push_back(node);
+			CNF_T lcnf, rcnf;
+			Node* lc = new Node(node->dec, node->val, lcnf);
+			Node* rc = new Node(node->dec, node->val, rcnf);
+			if (fake_propagation(node, lc, rc) == 0) {
+				local_stack[tid].push_back(lc);
+				local_stack[tid].push_back(rc);
+			}
 		}
 	}
 
 	for (int i = 0; i < NTHREADS; i++) {
+		cout << "stack " << i << endl;
 		while (!local_stack[i].empty()) {
 			Node* curr = local_stack[i].front();
 			for (int j = 0; j < 8; j++) cout << curr->dec.get(j);
 			cout << "/";
 			for (int j = 0; j < 8; j++) cout << curr->val.get(j);
 			cout << endl;
-			cout << curr->cnf;
+			for (CNF_T::iterator ii = curr->cnf.begin(); ii != curr->cnf.end(); ii++) {
+				for (CLAUSE_T::iterator ij = (*ii).begin(); ij != (*ii).end(); ij++) {
+					cout << *(ij) << " ";
+				}
+				cout << endl;
+			}
 			local_stack[i].pop_front();
 		}
 	}
