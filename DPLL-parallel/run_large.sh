93eb6ded3bbc082ba9dbafa3878eb0744666220d
yuejiewang <kathywangyuejie@gmail.com>
2021-04-12 00:53:47 +0800

    mod shell script

--- DPLL-parallel/run_large.sh
+++ DPLL-parallel/run_large.sh
@@ -8,6 +8,9 @@
 PATH=/home/edison/bin:/home/edison/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/work/tools/gcc-3.4.5-glibc-2.3.6/bin
 export PATH
 
+make clean
+make
+
 for file in ./large_input/*
 do
 	filename=${file%.*}
