eb540e388ee074eedb9219756b4ff016e626a43e
nspektor <nellie.spektor@gmail.com>
2021-04-16 09:57:08 -0400

    setup for benchmark

---
+++ benchmarks/benchmarks.py
@@ -0,0 +1,21 @@
+"""plingeling
+lingeling
+DPLL sequential
+DPLL parallel
+
+for each of those, varying the number of threads (1-64), time all the test cases and take an average
+
+benchmark.py expected behavior:
+ 1. take list of executables as commandline input
+ 2. run each executable on each input in benchmarks/inputs
+    - time each run and keep it in a file called benchmark_<executable>.txt in the format:
+        inputname   ms runtime
+
+
+
+
+
+
+take command line input for executables
+
+- benchmarks script (lorenzo to send plingeeling and lingeling instructions and sample cnf files to test with)"""
