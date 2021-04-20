57cfe1880a577663e615273a17313bbb4f530a36
nspektor <nellie.spektor@gmail.com>
2021-04-20 14:01:17 -0400

    add testing for all output directories

--- tests/test_correctness_of_outputs.py
+++ tests/test_correctness_of_outputs.py
@@ -3,7 +3,7 @@ import os
 from check_sat import *
 
 input_dir = "../input/"
-par_output_dir = "../output_parallel_p0/"
+par_output_dirs = ["../output_parallel_p0/", "../output_parallel_p1/", "../output_parallel_p2/","../output_parallel_p3/", "../output_parallel_p4/"]
 seq_output_dir = "../sequential_output/"
 
 input_files = os.listdir(input_dir)
@@ -21,11 +21,11 @@ class OutputCorrectnessTests(unittest.TestCase):
         self.assertEqual(False, valid)
 
     def test_one_parallel_sat(self):
-        valid = check_correctness(input_dir+"sat_fromAIclass.cnf", par_output_dir+"sat_fromAIclass.txt")
+        valid = check_correctness(input_dir+"sat_fromAIclass.cnf", par_output_dirs[1]+"sat_fromAIclass.txt")
         self.assertEqual(True, valid)
 
     def test_one_parallel_unsat(self):
-        valid = check_correctness(input_dir+"unsat_hole6.cnf", par_output_dir+"unsat_hole6.txt")
+        valid = check_correctness(input_dir+"unsat_hole6.cnf", par_output_dirs[1]+"unsat_hole6.txt")
         self.assertEqual(False, valid)
 
     def test_all_sequential_sat(self):
@@ -43,16 +43,18 @@ class OutputCorrectnessTests(unittest.TestCase):
             self.assertEqual(False, valid, file)
 
     def test_all_parallel_sat(self):
-        for file in sat_input_files:
-            base = file[0:-4]
-            valid = check_correctness(input_dir+file, par_output_dir+base+".txt")
-            self.assertEqual(True, valid, file)
+        for par_output_dir in par_output_dirs:
+            for file in sat_input_files:
+                base = file[0:-4]
+                valid = check_correctness(input_dir+file, par_output_dir+base+".txt")
+                self.assertEqual(True, valid, file)
 
     def test_all_parallel_unsat(self):
-        for file in unsat_input_files:
-            base = file[0:-4]
-            valid = check_correctness(input_dir+file, par_output_dir+base+".txt")
-            self.assertEqual(False, valid, file)
+        for par_output_dir in par_output_dirs:
+            for file in unsat_input_files:
+                    base = file[0:-4]
+                    valid = check_correctness(input_dir+file, par_output_dir+base+".txt")
+                    self.assertEqual(False, valid, file)
 
 
 if __name__ == '__main__':
